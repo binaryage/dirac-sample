5cd082bd4b57769a237c19791233020a92ec2d6e
Antonin Hildebrand <antonin@hildebrand.cz>
2016-05-29 17:52:04 +0200

    add project config for cider middleware experiments

--- project.clj
+++ project.clj
@@ -75,6 +75,29 @@
                                                    :source-map-timestamp true}}}}}
 
              ; --------------------------------------------------------------------------------------------------------------
+             :cider
+             {:dependencies [[cider/cider-nrepl "0.12.0"]]
+              :repl-options {:nrepl-middleware [cider.nrepl.middleware.apropos/wrap-apropos
+                                                cider.nrepl.middleware.classpath/wrap-classpath
+                                                cider.nrepl.middleware.complete/wrap-complete
+                                                cider.nrepl.middleware.debug/wrap-debug
+                                                cider.nrepl.middleware.format/wrap-format
+                                                cider.nrepl.middleware.info/wrap-info
+                                                cider.nrepl.middleware.inspect/wrap-inspect
+                                                cider.nrepl.middleware.macroexpand/wrap-macroexpand
+                                                cider.nrepl.middleware.ns/wrap-ns
+                                                cider.nrepl.middleware.pprint/wrap-pprint
+                                                cider.nrepl.middleware.pprint/wrap-pprint-fn
+                                                cider.nrepl.middleware.refresh/wrap-refresh
+                                                cider.nrepl.middleware.resource/wrap-resource
+                                                cider.nrepl.middleware.stacktrace/wrap-stacktrace
+                                                cider.nrepl.middleware.test/wrap-test
+                                                cider.nrepl.middleware.trace/wrap-trace
+                                                cider.nrepl.middleware.out/wrap-out
+                                                cider.nrepl.middleware.undef/wrap-undef
+                                                cider.nrepl.middleware.version/wrap-version]}
+              }
+             ; --------------------------------------------------------------------------------------------------------------
              :repl
              {:repl-options {:port             8230
                              :nrepl-middleware [dirac.nrepl/middleware]
@@ -119,6 +142,7 @@
                                   "cljsbuild" "once,"
                                   "shell" "scripts/dev-server.sh"]
             "repl17"             ["with-profile" "+base,+repl,+clojure17,+checkouts" "repl"]
+            "repl-cider"         ["with-profile" "+base,+repl,+checkouts,+cider" "repl"]
             "dev-figwheel"       ["with-profile" "+demo,+tests,+checkouts,+figwheel" "figwheel" "demo" "tests"]
             "auto-compile-tests" ["with-profile" "+tests,+checkouts" "cljsbuild" "auto"]
             "auto-compile-demo"  ["with-profile" "+demo,+checkouts" "cljsbuild" "auto"]
