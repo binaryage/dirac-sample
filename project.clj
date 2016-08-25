491ef3ca9b822c5545f622e6f4344dfe936dcb2e
Antonin Hildebrand <antonin@hildebrand.cz>
2016-08-25 19:21:52 +0200

    updates for dirac 0.6.4

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "https://github.com/binaryage/dirac-sample"
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.89"]
-                 [binaryage/devtools "0.7.2"]
-                 [binaryage/dirac "0.6.3"]
-                 [figwheel "0.5.4-7"]]
+                 [org.clojure/clojurescript "1.9.225"]
+                 [binaryage/devtools "0.8.1"]
+                 [binaryage/dirac "0.6.4"]
+                 [figwheel "0.5.5"]]
 
   :plugins [[lein-cljsbuild "1.1.3"]
             [lein-shell "0.5.0"]
             [lein-cooper "1.2.2"]
-            [lein-figwheel "0.5.4-7"]]
+            [lein-figwheel "0.5.5"]]
 
   ; =========================================================================================================================
 
@@ -35,8 +35,8 @@
              {:dependencies ^:replace [[org.clojure/clojure "1.7.0"]
                                        [org.clojure/tools.nrepl "0.2.12"]                                                     ; for some reason this is needed for Clojure 1.7
                                        [clojure-complete "0.2.4" :exclusions [org.clojure/clojure]]                           ; for some reason this is needed for Clojure 1.7
-                                       [org.clojure/clojurescript "1.9.89"]
-                                       [binaryage/devtools "0.7.2"]
+                                       [org.clojure/clojurescript "1.9.225"]
+                                       [binaryage/devtools "0.8.1"]
                                        [binaryage/dirac "0.6.3"]]}
 
              ; --------------------------------------------------------------------------------------------------------------
