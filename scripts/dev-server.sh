cf40acb623d1ce88692ad7515afa45fdc24f1e6a
Antonin Hildebrand <antonin@hildebrand.cz>
2016-08-31 11:54:11 +0200

    handle missing python in scripts/dev-server.sh
    Fixes https://github.com/binaryage/dirac/issues/35

--- scripts/dev-server.sh
+++ scripts/dev-server.sh
@@ -10,8 +10,14 @@ DEVSERVER_PORT=9977
 
 pushd "$DEVSERVER_ROOT"
 
-echo "starting HTTP server on port $DEVSERVER_PORT => http://localhost:$DEVSERVER_PORT"
+if [ which python > /dev/null 2>&1 ]; then
+  echo "starting HTTP server on port $DEVSERVER_PORT => http://localhost:$DEVSERVER_PORT"
+else
+  echo "Error: python does not seem to be installed on your PATH. We use python to start a simple HTTP server."
+  exit 3
+fi
 
-python -m SimpleHTTPServer "$DEVSERVER_PORT" 2> /dev/null
+python -m SimpleHTTPServer "$DEVSERVER_PORT" 2> /dev/null \
+  || echo "Error: failed to start 'python -m SimpleHTTPServer ...', do you have python properly installed?"
 
 popd
