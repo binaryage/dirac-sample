7400d128bba150fbb80302ff0d25c2355230c8db
Antonin Hildebrand <antonin@hildebrand.cz>
2018-05-28 12:42:58 +0200

    updates for dirac 1.2.35

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (def devtools-version "0.9.10")
-(def dirac-version "1.2.34")
+(def dirac-version "1.2.35")
 (def figwheel-version "0.5.16")
 (defproject binaryage/dirac-sample "0.1.0-SNAPSHOT"
   :description "An example integration of Dirac DevTools"
