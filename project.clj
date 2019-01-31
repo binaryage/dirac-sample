6c9dc6bf27be303e1f0cf5e7d0821de992f11c15
Antonin Hildebrand <antonin@hildebrand.cz>
2019-01-31 23:35:05 +0100

    updates for dirac 1.3.1

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (def devtools-version "0.9.10")
-(def dirac-version "1.3.0")
+(def dirac-version "1.3.1")
 (def figwheel-version "0.5.18")
 (defproject binaryage/dirac-sample "0.1.0-SNAPSHOT"
   :description "An example integration of Dirac DevTools"
   :url "https://github.com/binaryage/dirac-sample"
 
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.10.439"]
+                 [org.clojure/clojurescript "1.10.516"]
                  [nrepl/nrepl "0.5.3"]
                  [clojure-complete "0.2.5" :exclusions [org.clojure/clojure]]
                  [binaryage/devtools ~devtools-version]
