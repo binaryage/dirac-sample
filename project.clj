2fc452676eee9cdf25f946da9a022edc8b1958b8
Antonin Hildebrand <antonin@hildebrand.cz>
2019-07-15 22:39:53 +0200

    updates for dirac 1.3.9

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (def devtools-version "0.9.10")
-(def dirac-version "1.3.8")
+(def dirac-version "1.3.9")
 (def figwheel-version "0.5.19")
 (defproject binaryage/dirac-sample "0.1.0-SNAPSHOT"
   :description "An example integration of Dirac DevTools"
